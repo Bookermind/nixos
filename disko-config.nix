257e08b7734694d369abd2f403fd2659333ed8de
Simon Booker <simon.booker@zoho.eu>
2026-03-01 16:33:37 +0000

    Create disko-config.nix

---
+++ disko-config.nix
@@ -0,0 +1,61 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "cryptsys";
+                settings.allowDiscards = true;
+                content = {
+                  type = "lvm_pv";
+                  vg = "sys";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      sys = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [ "defaults" ];
+            };
+          };
+          home = {
+            size = "10G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
+            };
+          };
+        };
+      };
+    };
+  };
+}
