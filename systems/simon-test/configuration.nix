d9abad265fbd2d3427b909d8bdebbd98accb5f1f
Simon Booker <simon.booker@zoho.eu>
2026-03-04 22:50:14 +0000

    Update configuration.nix

--- systems/simon-test/configuration.nix
+++ systems/simon-test/configuration.nix
@@ -3,7 +3,6 @@
 {
   imports = [
     ./hardware-configuration.nix
-    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
     ./disko-config.nix
     inputs.home-manager.nixosModules.default
   ];
